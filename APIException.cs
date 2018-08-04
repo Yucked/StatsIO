623ff3dbdf1a6aa35aceb9041c83368f05e6b99f
Yivlx <vuxdev@gmail.com>
2018-08-04 11:15:49 -0400

    GlobalException -> APIException

---
+++ APIException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatsIO
+{
+    public class APIException : Exception
+    {
+        public APIException(string message) : base(message)
+        {
+            
+        }
+    }
+}
